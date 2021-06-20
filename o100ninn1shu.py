c832adafda150faed670e5aa506377b62c973c8f
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-06-20 10:41:28 +0900

    漢字表記、最小文字表示対応

--- o100ninn1shu.py
+++ o100ninn1shu.py
@@ -54,20 +54,25 @@ def answer_monitor():
 
 if __name__ == "__main__":
     # 句読み込み
-    kami_list, simo_list, author_list = [], [], []
+    kami_list, simo_list, kami_kana_list, simo_kana_list, author_list = [], [], [], [], []
     with open('./o100ninn1shu.csv', 'r', encoding='utf-8') as fin:
         for row in csv.DictReader(fin):
-            kami_list += [row['上の句(かな)']]
-            simo_list += [row['下の句(かな)']]
+            kami_list += [row['上の句']]
+            simo_list += [row['下の句']]
+            kami_kana_list += [row['上の句(かな)']]
+            simo_kana_list += [row['下の句(かな)']]
             author_list += [row['作者']]
 
     # 上の句の最小
-    min_kami = get_min_list(kami_list)
+    kami_kana_list_min = get_min_list(kami_kana_list)
+
+    # 下の句の最小
+    simo_kana_list_min = get_min_list(simo_kana_list)
 
     # 回答受付スレッド
     while True:
         # 問題選択(ランダムに5つ選ぶ)
-        selections = random.sample(range(len(simo_list)), SELECTIONS_NUM)
+        selections = random.sample(range(len(simo_kana_list)), SELECTIONS_NUM)
 
         # 解答選択(ランダムに1つ選ぶ)
         answer_index = random.choice(selections)
@@ -75,8 +80,16 @@ if __name__ == "__main__":
         # 整理
         kami = kami_list[answer_index]
         simo = simo_list[answer_index]
+        kami_kana = kami_kana_list[answer_index]
+        simo_kana = simo_kana_list[answer_index]
         author = author_list[answer_index]
         answer = selections.index(answer_index) + 1
+        kami_min = kami_kana_list_min[answer_index]
+        simo_min = simo_kana_list_min[answer_index]
+
+        # この問題の上の句の最小
+        kami_kana_list_min2 = get_min_list([kami_kana_list[i] for i in selections])
+        kami_min2 = kami_kana_list_min2[answer-1]
 
         # 問題表示
         print()
@@ -93,7 +106,7 @@ if __name__ == "__main__":
 
         # 上の句読み上げ
         question = ''
-        for i in kami:
+        for i in kami_kana:
             sys.stdout.write(i)
             sys.stdout.flush()
             time.sleep(0.25)
@@ -113,6 +126,14 @@ if __name__ == "__main__":
         print(f'[{author}]')
         print(f'{kami}')
         print(f'{simo}')
+        print()
+
+        tmp = re.sub(r'^(' + kami_min + r')', r'(\1) ', kami_kana)
+        kami_kana = re.sub(r'^\((' + kami_min2 + r')', r'(<\1> ', tmp)
+        print(f'{kami_kana}')
+
+        simo_kana = re.sub(r'^(' + simo_min + r')', r'(\1) ', simo_kana)
+        print(f'{simo_kana}')
 
         print()
         key = input('Enter:もう一度 q:終了 > ')
